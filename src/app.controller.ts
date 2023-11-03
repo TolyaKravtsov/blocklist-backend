f24f1d55be1f5a2573f47b0ace0373bf35f3a1a8
tolyakravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2023-11-03 17:08:58 +0100

    fix swagger results, remove tests

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+
+class helloWorldDto {
+  @ApiProperty()
+  message: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiOkResponse({
+    type: helloWorldDto,
+  })
+  getHello(): helloWorldDto {
+    return { message: this.appService.getHello() };
   }
 }

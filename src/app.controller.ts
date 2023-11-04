1414e0417fb5d5b5eb380e850d354dcc30410bc4
tolyakravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2023-11-04 14:28:31 +0100

    configure backend

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiOkResponse, ApiProperty } from '@nestjs/swagger';
+import { DbService } from './db/db.service';
 
 class helloWorldDto {
   @ApiProperty()
@@ -9,13 +10,18 @@ class helloWorldDto {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private dbService: DbService,
+  ) {}
 
   @Get()
   @ApiOkResponse({
     type: helloWorldDto,
   })
-  getHello(): helloWorldDto {
+  async getHello(): Promise<helloWorldDto> {
+    const users = await this.dbService.user.findFirst({});
+    console.log(users);
     return { message: this.appService.getHello() };
   }
 }

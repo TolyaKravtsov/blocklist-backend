f0534afc19f13e16b4e700f5d2431cd6cf1f27cb
tolyakravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2023-11-04 23:01:55 +0100

    replace to double quote

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('site blocker')
-    .setDescription('API for site blocker')
-    .setVersion('1.0')
-    .addTag('blocker')
+    .setTitle("site blocker")
+    .setDescription("API for site blocker")
+    .setVersion("1.0")
+    .addTag("blocker")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
+
+  app.use(cookieParser());
 
   await app.listen(3000);
 }

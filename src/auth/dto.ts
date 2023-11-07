c5c046e9cf5362becfef7a4f067775263949c8a9
tolyakravtsov <54136482+TolyaKravtsov@users.noreply.github.com>
2023-11-07 13:06:37 +0100

    implement validation

--- src/auth/dto.ts
+++ src/auth/dto.ts
@@ -1,17 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
 
 export class SignUpBodyDto {
   @ApiProperty({ example: "test@gmail.com" })
+  @IsEmail()
   email: string;
   @ApiProperty({ example: "1234" })
+  @IsNotEmpty()
   password: string;
 }
 
 export class SignInBodyDto {
   @ApiProperty({ example: "test@gmail.com" })
+  @IsEmail()
   email: string;
 
   @ApiProperty({ example: "1234" })
+  @IsNotEmpty()
   password: string;
 }
 
